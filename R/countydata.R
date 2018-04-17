79864825385cbb4634089f576f54b5ce9ed412a2
Strochak <SStrochak@urban.org>
2018-04-17 15:57:13 -0400

    Added data for county examples

---
+++ R/countydata.R
@@ -0,0 +1,20 @@
+#' County data for mapping
+#'
+#' County-level data to use for example maps.
+#' Includes county FIPS code to match with county shapefile data.
+#'
+#' @source Urban Institute Sloan ADRF Database,
+#'  \url{https://adrf.urban.org}
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{year}{Year of data}
+#' \item{county_fips}{Five-digit FIPS code}
+#' \item{hhhpop}{Household population}
+#' \item{horate}{Homeownership rate}
+#' \item{medhhincome}{Median household income}
+#' }
+'counties'
+
+#' @importFrom tibble tibble
+NULL
