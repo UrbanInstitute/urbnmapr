6a662a78f1b5d715d59309a047c5568a6d12d4a7
Strochak <SStrochak@urban.org>
2018-07-06 16:39:02 -0400

    finish territories and add labels

--- data-raw/county-territories.R
+++ data-raw/county-territories.R
@@ -39,6 +39,11 @@ counties_sp <- readOGR(dsn = temp_dir, layer = shape_base, verbose = FALSE) %>%
   # Convert from NAD83 to EPSG 2163 (US National Atlas Equal Area)
   spTransform(CRS("+init=epsg:2163"))
 
+
+exclude_names <- c("Alaska", "Hawaii", "Puerto Rico", "American Samoa", "Guam", "Commonwealth of the Northern Mariana Islands", "United States Virgin Islands")
+exclude_codes <- c("02", "15", "60", "66", "69", "72", "78")
+testmap <- counties_sp[!counties_sp$STATEFP %in% exclude_codes, ]
+
 # alaska ------------------------------------------------------------------
 
 alaska <- counties_sp[counties_sp$STATEFP == "02", ] %>%
@@ -58,28 +63,44 @@ hawaii <- counties_sp[counties_sp$STATEFP == "15", ] %>%
 proj4string(hawaii) <- proj4string(counties_sp)
 
 
-exclude_names <- c("Alaska", "Hawaii", "Puerto Rico", "American Samoa", "Guam", "Commonwealth of the Northern Mariana Islands", "United States Virgin Islands")
-exclude_codes <- c("02", "15", "60", "66", "69", "72", "78")
-testmap <- counties_sp[!counties_sp$STATEFP %in% exclude_codes, ]
 
 
 # guam --------------------------------------------------------------------
 
 guam <- counties_sp[counties_sp$STATEFP %in% c("66"), ] %>%
-  transform_state(rot = -35, scale = 0.2, shift = c(-180000, -2520000))
+  transform_state(rot = -35, scale = 0.2, shift = c(-180000, -2700000))
 
 proj4string(guam) <- proj4string(counties_sp)
 
-test <- rbind(testmap, hawaii, alaska, guam)
-plot(test)
 
 
 # puerto rico -------------------------------------------------------------
 
 puerto_rico <- counties_sp[counties_sp$STATEFP %in% c("72"), ] %>%
-  transform_state(rot = 0, scale = .5, shift = c(200000, -2500000))
+  transform_state(rot = 20, scale = .5, shift = c(200000, -2500000))
 
 proj4string(puerto_rico) <- proj4string(counties_sp)
+
+
+
+# virgin islands ----------------------------------------------------------
+
+virgin_islands <- counties_sp[counties_sp$STATEFP %in% c("78"), ] %>%
+  transform_state(rot = 0, scale = 0.2, shift = c(1550000, -2700000))
+
+proj4string(virgin_islands) <- proj4string(counties_sp)
+
+
+
+# mariana islands ---------------------------------------------------------
+
+mariana_islands <- counties_sp[counties_sp$STATEFP %in% c("69"), ] %>%
+  transform_state(rot = -35, scale = 0.3, shift = c(2150000, -2450000))
+
+proj4string(mariana_islands) <- proj4string(counties_sp)
+
+test <- rbind(testmap, hawaii, alaska, guam, puerto_rico, virgin_islands, mariana_islands)
+plot(test)
 
 
 
@@ -87,53 +108,56 @@ proj4string(puerto_rico) <- proj4string(counties_sp)
 
 american_samoa <- counties_sp[counties_sp$STATEFP %in% c("60"), ] %>%
   transform_state(rot = 0, scale = 0.1, shift = c(-1250000, -5050000))
+proj4string(american_samoa) <- proj4string(counties_sp)
 
-as1 <- american_samoa[american_samoa$NAME %in% c("Manu'a"), ] %>%
-  transform_state(rot = 0, scale = 0.5, shift = c(-1250000, -5050000))
-
-as1 <- american_samoa[american_samoa$NAME %in% c("Acadia"), ] %>%
-  transform_state(rot = 0, scale = 0.1, shift = c(-1250000, -5050000))
-
-
+# treat islands differently
+as1 <- american_samoa[!american_samoa$NAME %in% c("Rose Island", "Swains Island", "Manu'a"), ] %>%
+  transform_state(rot = 0, scale = 1.2, shift = c(950000, -2350000))
 proj4string(as1) <- proj4string(counties_sp)
 
-test <- rbind(testmap, hawaii, alaska, guam, puerto_rico, as1)
+as2 <- american_samoa[american_samoa$NAME %in% c("Manu'a"), ] %>%
+  transform_state(rot = 0, scale = 1.2, shift = c(1450000, -2800000))
+proj4string(as2) <- proj4string(counties_sp)
+
+american_samoa <- rbind(as1, as2) %>%
+  # angle and size need to be slightly adjusted
+  transform_state(rot = 15, scale = 1, shift = c(1000000,-3000000))
+proj4string(american_samoa) <- proj4string(counties_sp)
+
+
+
+test <- rbind(testmap, hawaii, alaska, guam, puerto_rico, virgin_islands, mariana_islands,
+              american_samoa)
 plot(test)
 
-# virgin islands ----------------------------------------------------------
 
-virgin_islands <- fifty_states_sp[fifty_states_sp$NAME %in% c("United States Virgin Islands"), ] %>%
-  transform_state(rot = 0, scale = 0.1, shift = c(2150000, -1600000))
+# combine and convert to tibble -------------------------------------------
 
-proj4string(virgin_islands) <- proj4string(fifty_states_sp)
-
-# guam --------------------------------------------------------------------
-
-guam <- fifty_states_sp[fifty_states_sp$NAME %in% c("Guam"), ] %>%
-  transform_state(rot = -35, scale = 0.3, shift = c(-2600000, -700000))
-
-proj4string(guam) <- proj4string(fifty_states_sp)
-
-# american samoa ----------------------------------------------------------
-
-american_samoa <- fifty_states_sp[fifty_states_sp$NAME %in% c("American Samoa"), ] %>%
-  transform_state(rot = -35, scale = 0.15, shift = c(-4250000, -1450000))
-
-as1 <-
-
-proj4string(american_samoa) <- proj4string(fifty_states_sp)
-
-# mariana islands ---------------------------------------------------------
-
-mariana_islands <- fifty_states_sp[fifty_states_sp$NAME %in% c("Commonwealth of the Northern Mariana Islands"), ] %>%
-  transform_state(rot = -35, scale = 0.1, shift = c(-3600000, -1250000))
-
-proj4string(mariana_islands) <- proj4string(fifty_states_sp)
-
-mariana_islands <- mariana_islands %>%
-  # transforms into longitude/latitude
-  spTransform(CRS("+init=epsg:4326")) %>%  # prime meridian/equator
-  tidy(region = "NAME") %>%
+territories_county <-
+  rbind(alaska, hawaii, alaska, guam, puerto_rico, virgin_islands,
+        mariana_islands, american_samoa) %>%
+  # convert from EPSG2163 to (US National Atlas Equal Area) WGS84
+  spTransform(CRS("+init=epsg:4326")) %>%
+  tidy(region = "GEOID") %>%
+  rename(county_fips = id) %>%
   as_tibble() %>%
-  filter(piece %in% 1:2)
+  # get_county_fips() found in utils.R
+  left_join(get_county_fips(), by = "county_fips")
 
+
+
+# test merge with urbnmapr::counties --------------------------------------
+
+
+x <- rbind(territories_county,
+           filter(urbnmapr::counties, !state_name %in% c("Alaska", "Hawaii")))
+
+urbnthemes::set_urban_defaults("print")
+
+ggplot() +
+  geom_polygon(data = x, mapping = aes(x = long, y = lat, group = group),
+               fill = "white", color = "black") +
+  urbnthemes::theme_urban_map() +
+  geom_text(data = territories_labels, mapping = aes(x = long, y = lat,
+                                                     label = state_name)) +
+  coord_map(projection = "albers", lat0 = 39, lat1 = 45)
