704bb514e6455441e065495aa6f47d3572bcde9e
awunderground <aaronrwilliams100@gmail.com>
2018-07-09 21:54:15 -0400

    Resolve all travis-ci errors and warnings after adding territories.

---
+++ R/ccdfmap.R
@@ -0,0 +1,12 @@
+#' Child Care and Development Fund map data
+#'
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{long, lat}{Longitude and latitude}
+#' \item{state_name, state_abbv, state_fips}{State name, state abbreviation, two-digit FIPS code}
+#' }
+'ccdfmap'
+
+#' @importFrom tibble tibble
+NULL
---
+++ R/states_labels.R
@@ -0,0 +1,13 @@
+#' States labels data
+#'
+#' States labels data
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{long, lat}{Longitude and latitude}
+#' \item{state_name, state_abbv}{State name and state abbreviation}
+#' }
+'states_labels'
+
+#' @importFrom tibble tibble
+NULL
